7ff8d3d0912cab78d5e89c3a3ad69e2199d070ba
AlexandreJunior <sergio.silva@mercadofavo.com>
2021-08-05 22:31:20 -0300

    godoenv inserted

--- jobs.go
+++ jobs.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
-func test() {
+func test(envFileName string) {
+	err := godotenv.Load(envFileName)
+	if err != nil {
+		fmt.Println("\033[31mError: Shared Package dont founded " + envFileName + " file\033[0m")
+	}
 	fmt.Println("TESTE JOBS: " + os.Getenv("DB_HOST"))
 }
 
