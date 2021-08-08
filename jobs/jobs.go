bb4108bc3492deabb16ed2fb3e8caf8bddec48da
AlexandreJunior <sergio.silva@mercadofavo.com>
2021-08-08 09:36:54 -0300

    package jobs inserted

--- jobs/jobs.go
+++ jobs/jobs.go
@@ -1,4 +1,4 @@
-package main
+package jobs
 
 import (
 	"fmt"
@@ -7,21 +7,10 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func test(envFileName string) {
+func Test(envFileName string) {
 	err := godotenv.Load(envFileName)
 	if err != nil {
 		fmt.Println("\033[31mError: Shared Package dont founded " + envFileName + " file\033[0m")
 	}
 	fmt.Println("TESTE JOBS: " + os.Getenv("DB_HOST"))
-}
-
-func main() {
-	var execute string = ""
-	fmt.Println(execute)
-}
-
-func Job() map[string]interface{} {
-	jobs := make(map[string]interface{})
-	jobs["Test"] = test
-	return jobs
 }
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/sergiodii/cron-go-jobs-function/jobs"
+)
+
+func main() {
+	//localToExecution
+}
+
+func noFunction() {
+	jobs.Test("")
+}
